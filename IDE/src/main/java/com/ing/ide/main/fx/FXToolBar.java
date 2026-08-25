2b21bfa2e9e3e98d2e6b5e238f3a5183158c377b
Mary Joyce Clarence Navarro <mjc.navarro28@gmail.com>
2026-08-25 15:16:31 +0800

    rebase and disable AI Assistant and Archetype download

--- IDE/src/main/java/com/ing/ide/main/fx/FXToolBar.java
+++ IDE/src/main/java/com/ing/ide/main/fx/FXToolBar.java
@@ -67,7 +67,7 @@ public class FXToolBar extends JFXPanel {
                 new Separator(),
                 createAPITesterButton(),
                 createPerfStudioButton(),
-                createSpacer(),
+                createSpacer()
                 // createAICopilotButton()
                 //, createDarkModeToggle()
             );
