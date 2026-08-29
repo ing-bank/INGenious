8a79791fcd2e0e3e68f440a4482c4afae49f3024
juliusborja <juliuschristopherborja@gmail.com>
2026-08-30 01:36:22 +0800

    Enable copy and paste actions and enable short cut keys for fixed columns

--- IDE/src/main/java/com/ing/ide/main/utils/keys/ClipboardKeyAdapter.java
+++ IDE/src/main/java/com/ing/ide/main/utils/keys/ClipboardKeyAdapter.java
@@ -1,10 +1,12 @@
 package com.ing.ide.main.utils.keys;
 
+import com.ing.ide.main.utils.table.XTable;
 import com.ing.ide.main.utils.table.XTableUtils;
+import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import javax.swing.Action;
 import javax.swing.JTable;
-import org.jline.utils.Log;
 
 /**
  *
@@ -24,17 +26,53 @@ public class ClipboardKeyAdapter extends KeyAdapter {
                 case KeyEvent.VK_C:
                     // Copy
                     cancelEditing();
-                    XTableUtils.copyToClipboard(table, false);
+                    Action copyAction = table.getActionMap().get("Copy");
+                    if (copyAction == null) {
+                        copyAction = table.getActionMap().get("copy");
+                    }
+                    if (copyAction != null) {
+                        copyAction.actionPerformed(
+                            new ActionEvent(table, ActionEvent.ACTION_PERFORMED, "Copy")
+                        );
+                    } else if (table instanceof XTable) {
+                        ((XTable) table).copy();
+                    } else {
+                        XTableUtils.copyToClipboard(table, false);
+                    }
                     break;
                 case KeyEvent.VK_X:
                     // Cut
                     cancelEditing();
-                    XTableUtils.copyToClipboard(table, true);
+                    Action cutAction = table.getActionMap().get("Cut");
+                    if (cutAction == null) {
+                        cutAction = table.getActionMap().get("cut");
+                    }
+                    if (cutAction != null) {
+                        cutAction.actionPerformed(
+                            new ActionEvent(table, ActionEvent.ACTION_PERFORMED, "Cut")
+                        );
+                    } else if (table instanceof XTable) {
+                        ((XTable) table).cut();
+                    } else {
+                        XTableUtils.copyToClipboard(table, true);
+                    }
                     break;
                 case KeyEvent.VK_V:
                     // Paste
                     cancelEditing();
-                    XTableUtils.pasteFromClipboard(table);
+                    Action pasteAction = table.getActionMap().get("Paste");
+                    if (pasteAction == null) {
+                        pasteAction = table.getActionMap().get("paste");
+                    }
+                    if (pasteAction != null) {
+                        pasteAction.actionPerformed(
+                            new ActionEvent(table, ActionEvent.ACTION_PERFORMED, "Paste")
+                        );
+                    } else if (table instanceof XTable) {
+                        ((XTable) table).paste();
+                    } else {
+                        XTableUtils.pasteFromClipboard(table);
+                    }
                     break;
                 default:
                     break;
