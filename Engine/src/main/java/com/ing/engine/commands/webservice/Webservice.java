04714862b9e85f402fbe46e000ebcbc277f2007c
juliusborja <37961839+juliusborja@users.noreply.github.com>
2026-09-22 15:02:30 +0800

    Remove hardcoded value for storeJsonElementCount
    * Remove hardcoded value for storeJsonElementCount
    * Remove hardcoded result for storeJsonElementCountInDataSheet

--- Engine/src/main/java/com/ing/engine/commands/webservice/Webservice.java
+++ Engine/src/main/java/com/ing/engine/commands/webservice/Webservice.java
@@ -1154,7 +1154,7 @@ public class Webservice extends GeneralWebservice {
                     System.out.println(
                         "Updating value in SubIteration " + userData.getSubIteration()
                     );
-                    int actualObjectCountInteger = 1; //getJsonElementCount();
+                    int actualObjectCountInteger = getJsonElementCount();
                     String actualObjectCount = Integer.toString(actualObjectCountInteger);
                     addVar(variableName, actualObjectCount);
                     Report.updateTestLog(
@@ -1258,7 +1258,7 @@ public class Webservice extends GeneralWebservice {
                     );
                     String sheetName = strObj.split(":", 2)[0];
                     String columnName = strObj.split(":", 2)[1];
-                    int actualObjectCountInteger = 1; //getJsonElementCount();
+                    int actualObjectCountInteger = getJsonElementCount();
                     String actualObjectCount = Integer.toString(actualObjectCountInteger);
                     userData.putData(sheetName, columnName, actualObjectCount);
                     Report.updateTestLog(
